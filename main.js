f2da4a21f2538ce6bba0fdddd3136e46fdcd85f9
jevonthomas <jevon.g.thomas@gmail.com>
2017-06-07 23:26:07 -0500

    Changed the nav bar to completely disappear until the menu button is clicked. Changed the font and added a blue overlay to the background image.

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ console.log(hiddenMenu);
 let hamburgerMenu = document.getElementById("menu_icon");
 console.log(hamburgerMenu);
 
+let navigation = document.getElementsByClassName("navigation");
+
+let mainHeader = document.getElementsByClassName("main_header");
+
 // add click event to the menu icon
 hamburgerMenu.addEventListener("click", function() {
 // on click, toggle the ID of the nav links
 	hiddenMenu[0].classList.toggle("menu_show");
+	navigation[0].classList.toggle("menu_show");
+	mainHeader[0].classList.toggle("menu_show");
 });
